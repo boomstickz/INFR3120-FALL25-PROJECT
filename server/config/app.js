6d99aeb3c43260d64c4fee513551f87e077a1f19
sekitch42 <sarah.kitchen@ontariotechu.net>
2025-11-15 15:16:19 -0500

    changed mongodb from local added env to hide password

--- server/config/app.js
+++ server/config/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// added mongoose for mongodb collection
 let mongoose = require('mongoose');
 let DB = require('./db');
 
@@ -19,34 +20,37 @@ mongoDB.once('open',()=>{
   console.log('Connected to the MongoDB');
 })
 
-// view engine setup
+// view engine setup (point to ../views from /config)
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
+// middleware stack
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// static folders (back out of /server to project root)
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
+
+// route mapping
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/characters',characterRouter);
+app.use('/characters', characterRouter);                // ⬅ IMPORTANT
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error', {title:'Error'});
+  res.render('error');
 });
 
 module.exports = app;
