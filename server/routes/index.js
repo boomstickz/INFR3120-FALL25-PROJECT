f467ccde8aaf96038fd0296a01f25a49731fc711
boomstickz <ali.alam@ontariotechu.net>
2025-11-25 18:54:35 -0500

    feat(routes): add /about route to render About page

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function (req, res) {
   res.sendFile('index.html', { root: 'public' });
 });
 
+// About page
+router.get('/about', function (req, res) {
+  res.render('about', { title: 'About | Forge My Hero' });
+});
+
 module.exports = router;
