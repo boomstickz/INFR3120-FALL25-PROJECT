e3650514e30c9f962739e55e6d8ce81e97dac855
boomstickz <ali.alam@ontariotechu.net>
2025-12-06 17:10:32 -0500

    fixed add/edit character traits: unify add/edit payload handling and persist all form inputs across create/update routes

--- server/routes/character.js
+++ server/routes/character.js
@@ -8,6 +8,72 @@ let Character = require('../models/character');
 //  auth middleware
 const { ensureLoggedIn } = require('../middleware/auth');
 
+// Collects character fields from the request body so add and edit routes stay in sync
+const buildCharacterPayload = (req) => ({
+  characterName: req.body.characterName,
+  classLevel: req.body.classLevel,
+  background: req.body.background,
+  race: req.body.race,
+  alignment: req.body.alignment,
+
+  inspiration: !!req.body.inspiration,
+  proficiencyBonus: req.body.proficiencyBonus,
+
+  strengthScore: req.body.strengthScore,
+  strengthMod: req.body.strengthMod,
+  dexterityScore: req.body.dexterityScore,
+  dexterityMod: req.body.dexterityMod,
+  constitutionScore: req.body.constitutionScore,
+  constitutionMod: req.body.constitutionMod,
+  intelligenceScore: req.body.intelligenceScore,
+  intelligenceMod: req.body.intelligenceMod,
+  wisdomScore: req.body.wisdomScore,
+  wisdomMod: req.body.wisdomMod,
+  charismaScore: req.body.charismaScore,
+  charismaMod: req.body.charismaMod,
+
+  saveStrength: req.body.saveStrength,
+  saveDexterity: req.body.saveDexterity,
+  saveConstitution: req.body.saveConstitution,
+  saveIntelligence: req.body.saveIntelligence,
+  saveWisdom: req.body.saveWisdom,
+  saveCharisma: req.body.saveCharisma,
+
+  skillAcrobatics: req.body.skillAcrobatics,
+  skillAnimalHandling: req.body.skillAnimalHandling,
+  skillArcana: req.body.skillArcana,
+  skillAthletics: req.body.skillAthletics,
+  skillDeception: req.body.skillDeception,
+  skillHistory: req.body.skillHistory,
+  skillInsight: req.body.skillInsight,
+  skillIntimidation: req.body.skillIntimidation,
+  skillInvestigation: req.body.skillInvestigation,
+  skillMedicine: req.body.skillMedicine,
+  skillNature: req.body.skillNature,
+  skillPerception: req.body.skillPerception,
+  skillPerformance: req.body.skillPerformance,
+  skillPersuasion: req.body.skillPersuasion,
+  skillReligion: req.body.skillReligion,
+  skillSleightOfHand: req.body.skillSleightOfHand,
+  skillStealth: req.body.skillStealth,
+  skillSurvival: req.body.skillSurvival,
+
+  passivePerception: req.body.passivePerception,
+  passiveInvestigation: req.body.passiveInvestigation,
+  passiveInsight: req.body.passiveInsight,
+
+  armor: req.body.armor,
+  weapons: req.body.weapons,
+  languages: req.body.languages,
+
+  armorClass: req.body.armorClass,
+  initiative: req.body.initiative,
+
+  maxHP: req.body.maxHP,
+  currentHP: req.body.currentHP,
+  tempHP: req.body.tempHP,
+});
+
 // get route for the read character list - read operation
 router.get('/', async (req, res, next) => {
   try {
@@ -48,17 +114,10 @@ router.get('/add', ensureLoggedIn, async (req, res, next) => {
 // post route for processing add page - create operation
 router.post('/add', ensureLoggedIn, async (req, res, next) => {
   try {
-    let newCharacter = Character({
-      "characterName": req.body.characterName,
-      "classLevel": req.body.classLevel,
-      "background": req.body.background,
-      "race": req.body.race,
-      "alignment": req.body.alignment,
-    });
+    let newCharacter = Character(buildCharacterPayload(req));
     Character.create(newCharacter).then(() => {
       res.redirect('/characters');
     });
-
   }
   catch (err) {
     console.error(err);
@@ -90,14 +149,10 @@ router.get('/edit/:id', ensureLoggedIn, async (req, res, next) => {
 router.post('/edit/:id', ensureLoggedIn, async (req, res, next) => {
   try {
     let id = req.params.id;
-    let updateCharacter = Character({
-      "_id": id,
-      "characterName": req.body.characterName,
-      "classLevel": req.body.classLevel,
-      "background": req.body.background,
-      "race": req.body.race,
-      "alignment": req.body.alignment,
-    });
+    let updateCharacter = {
+      _id: id,
+      ...buildCharacterPayload(req)
+    };
     Character.findByIdAndUpdate(id, updateCharacter).then(() => {
       res.redirect("/characters");
     });
