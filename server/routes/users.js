6d752656f0532f1c6eb3249cb17c4daf7fc5efb4
boomstickz <ali.alam@ontariotechu.net>
2025-12-06 22:06:35 -0500

    PROFILE IMAGE FEATURE: add route endpoint for handling profile image upload

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 const authController = require('../controllers/authController');
+const profileController = require('../controllers/profileController');
 
 // GET /users/login
 router.get('/login', authController.displayLoginPage);
@@ -17,5 +18,8 @@ router.post('/register', authController.processRegister);
 
 // POST /users/logout
 router.post('/logout', authController.processLogout);
+
+// POST /users/profile-picture
+router.post('/profile-picture', profileController.uploadProfilePicture);
 
 module.exports = router;
